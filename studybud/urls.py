d4e06fe3fab3f1b096db205512f09bf0c04d1f20
walaa alkasim <walaa.alkasim@hotmail.com>
2024-04-24 20:27:59 +0200

    first commit

---
+++ studybud/urls.py
@@ -0,0 +1,12 @@
+
+from django.contrib import admin
+from django.http import HttpResponse
+from django.urls import path, include
+
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', include('base.urls'))
+
+
+]
